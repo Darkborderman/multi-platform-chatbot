137c069bb80c5ed0bb3466860c425d5db5f1f337
darkborderman <reastw1234@gmail.com>
2019-12-09 00:00:08 +0800

    chore: Remove unused assets

--- others/callback_github.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    print(json.dumps(event,indent=4))
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
