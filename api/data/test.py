9dbf93fee4416a6626290530e7d1219001c584ab
darkborderman <reastw1234@gmail.com>
2020-01-01 16:19:06 +0800

    chore: Update test set

--- api/data/test.py
+++ api/data/test.py
@@ -21,7 +21,7 @@ TEST_LIST = [
         ''
     ),
     create_test_item(
-        ' Amazon Web Services is responsible for protecting the global infrastructure that runs all of the services offered in the AWS cloud. This infrastructure is comprised of: ',
+        'Amazon Web Services is responsible for protecting the global infrastructure that runs all of the services offered in the AWS cloud. This infrastructure is comprised of: ',
         [
             'The hardware, operational software, networking, and facilities that run AWS services.',
             'The facilities that run AWS services.',
@@ -31,5 +31,29 @@ TEST_LIST = [
         ['concept', 'security'],
         ['1'],
         ''
+    ),
+    create_test_item(
+        'AWS Key Management Service supports two kinds of keys.  Which of the following are the keys supported by AWS Key Management Service? ',
+        [
+            'Security keys and Network keys',
+            'Master keys and data keys',
+            'Memory keys and data keys',
+            'Encrypt keys and decrypt keys'
+        ],
+        ['security', 'KMS'],
+        ['2'],
+        ''
+    ),
+    create_test_item(
+        'By default, how many Amazon RDS DB instances can a customer have running?',
+        [
+            '20',
+            '30',
+            '40',
+            '100'
+        ],
+        ['datbase', 'RDS'],
+        ['3'],
+        'By default, customers are allowed to have up to a total of 40 Amazon RDS DB instances. Of those 40, up to 10 can be Oracle or SQL Server DB Instances under the License Included model. All 40 can be used for Amazon Aurora, MySQL, MariaDB, Oracle, SQL Server, or PostgreSQL under the BYOL model.'
     )
 ]
